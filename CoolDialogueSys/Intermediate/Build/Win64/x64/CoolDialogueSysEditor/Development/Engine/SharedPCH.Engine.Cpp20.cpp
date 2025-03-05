291a076c75efb5cc3c4b35b049033dea9192e13c
Xavier Olmstead <xavier.olmstead@mymail.champlain.edu>
2025-03-05 12:51:06 -0500

    added initial setup

--- CoolDialogueSys/Intermediate/Build/Win64/x64/CoolDialogueSysEditor/Development/Engine/SharedPCH.Engine.Cpp20.cpp
+++ CoolDialogueSys/Intermediate/Build/Win64/x64/CoolDialogueSysEditor/Development/Engine/SharedPCH.Engine.Cpp20.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.41.34123
+// Compiler: 14.38.33141
 #include "C:/Users/xavier.olmstead/repos/Dialogue-System/CoolDialogueSys/Intermediate/Build/Win64/x64/CoolDialogueSysEditor/Development/Engine/SharedPCH.Engine.Cpp20.h"
